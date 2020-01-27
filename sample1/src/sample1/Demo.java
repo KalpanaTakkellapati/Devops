af7f4491227721136603eada4cf77d668871e1e6
KalpanaTakkellapati <60207817+KalpanaTakkellapati@users.noreply.github.com>
2020-01-27 10:15:34 +0530

    Add files via upload

---
+++ sample1/src/sample1/Demo.java
@@ -0,0 +1,13 @@
+package sample1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,053,4};
+		int b[][]= {{1,2,4},{2,2,1},{0,43,2}};
+		System.out.println(a[3]==b[0][2]);
+		System.out.print(" "+(a[2]==b[2][1]));
+		
+	}
+
+}
